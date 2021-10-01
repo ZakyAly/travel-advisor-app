b615c428fdbb9868ab1b06b1c010de9b9e7500e6
zaky aly <zaky3ly@gmail.com>
2021-10-01 20:30:28 +0200

    adding Map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -13,7 +13,7 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBNgllNiB1mITQSDG4DluQHa6keqWFvABY" }}
+        bootstrapURLKeys={{ key: "" }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
