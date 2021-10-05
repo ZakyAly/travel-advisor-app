aba0c220098990c351fa55eebe6c8e45d6f3793f
zaky aly <zaky3ly@gmail.com>
2021-10-05 12:10:46 +0200

    cleaning code

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,24 @@
 import axios from "axios";
 
-// const URL =
-// "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-// "https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary";
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-export const getPlacesData = async (type, sw, ne) => {
+export const getPlacesData = async (sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(
-      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
-      {
-        params: {
-          bl_latitude: sw.lat,
-          bl_longitude: sw.lng,
-          tr_latitude: ne.lat,
-          tr_longitude: ne.lng,
-        },
-        headers: {
-          "x-rapidapi-key":
-            "64fca77853mshcbc9c264eaa9c1cp17c175jsn25ab313383f1",
-          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-        },
-      }
-    );
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "x-rapidapi-key": "64fca77853mshcbc9c264eaa9c1cp17c175jsn25ab313383f1",
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+      },
+    });
 
     return data;
   } catch (err) {
