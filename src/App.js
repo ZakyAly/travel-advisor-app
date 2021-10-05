e6d6bdfd87030c43799dcf699e0ec6d266efde3e
zaky aly <zaky3ly@gmail.com>
2021-10-05 12:36:31 +0200

    fixing getPlacesData

--- src/App.js
+++ src/App.js
@@ -21,9 +21,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    getPlacesData().then((data) => {
-      // bounds.sw,
-      // bounds.ne
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
       console.log(data);
       setPlaces(data);
     });
