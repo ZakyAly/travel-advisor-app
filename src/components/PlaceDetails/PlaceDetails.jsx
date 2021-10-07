d3dd328e297a55b18d25f4b9da1de43e569101c7
zaky aly <zaky3ly@gmail.com>
2021-10-07 15:13:32 +0200

    adding Rating to PlaceDetails

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -11,7 +11,7 @@ import {
 } from "@material-ui/core";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PhoneIcon from "@material-ui/icons/Phone";
-import { Rating } from "@material-ui/lab/Rating";
+import Rating from "@material-ui/lab/Rating";
 
 import useStyles from "./styles.js";
 
@@ -33,6 +33,12 @@ const PlaceDetails = ({ place }) => {
         <Typography gutterBottom variant="h5">
           {place.name}
         </Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Rating value={Number(place.rating)} readOnly />
+          <Typography gutterBottom variant="subtitle1">
+            out of {place.num_reviews}
+          </Typography>
+        </Box>
         <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Price</Typography>
           <Typography gutterBottom variant="subtitle1">
