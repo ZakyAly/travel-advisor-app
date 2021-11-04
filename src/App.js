049a8557bab835952d5c03a3f68b1ca1dd3ca7c8
zaky aly <zaky3ly@gmail.com>
2021-11-04 12:40:06 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
-import { getPlacesData } from "./api/index";
+import { getPlacesData, getWeatherData } from "./api/index";
 import Header from "./components/Header/Header.jsx";
 import List from "./components/List/List.jsx";
 import Map from "./components/Map/Map.jsx";
 
 const App = () => {
+  const [rating, setRating] = useState("");
+  const [type, setType] = useState("restaurants");
+
   const [places, setPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
   const [filteredPlaces, setFilteredPlaces] = useState([]);
-  const [childClicked, setChildClicked] = useState(null);
 
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
 
-  const [isLoading, setisLoading] = useState(false);
-
-  const [type, setType] = useState("restaurants");
-  const [rating, setRating] = useState("");
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -28,33 +29,38 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    const filteredPlaces = places.filter(
-      (place) => Number(place.rating) > rating
-    );
+    const filtered = places.filter((place) => Number(place.rating) > rating);
 
-    setFilteredPlaces(filteredPlaces);
-  }, [rating]);
+    setFilteredPlaces(filtered);
+  }, [rating, places]);
 
   useEffect(() => {
-    setisLoading(true);
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
 
-    getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
-      setPlaces(data);
-      setFilteredPlaces([]);
-      setisLoading(false);
-    });
-  }, [type, coordinates, bounds]);
+      getWeatherData(coordinates.lat, coordinates.lng).then((data) =>
+        setWeatherData(data)
+      );
+
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setRating("");
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds, coordinates]);
 
   return (
     <>
       <CssBaseline />
-      <Header />
+      <Header setCoordinates={setCoordinates} />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
           <List
-            isLoading={isLoading}
-            childClicked={childClicked}
             places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
             type={type}
             setType={setType}
             rating={rating}
@@ -68,6 +74,7 @@ const App = () => {
             coordinates={coordinates}
             places={filteredPlaces.length ? filteredPlaces : places}
             setChildClicked={setChildClicked}
+            weatherData={weatherData}
           />
         </Grid>
       </Grid>
